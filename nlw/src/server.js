3e19474da68eb9ee1cc3010344a90d5fe3a818b2
Wanderson Vicente <62729631+wandersonvicente@users.noreply.github.com>
2020-10-25 16:24:08 -0300

    nlw
    backend config
    node js
    npm node_modules

--- nlw/src/server.js
+++ nlw/src/server.js
@@ -1,17 +1,20 @@
+
 // importar dependência
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 
 // iniciando o express
 const server = express()
 
 
 // criar uma rota
-server.get('/', (request, response)=> {
+server.get('/', ()=> {
     //console.log(path.join(__dirname, 'views'))
     //return response.sendFile(__dirname)
-    console.log(__dirname)
+    //console.log(__dirname)
+    console.log('oi direto do backend')
 })
 
 // ligar o servidor
 server.listen(5500)
+
